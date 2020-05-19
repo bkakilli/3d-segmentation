9d20fa155c2171aaaae5178d01e3aaa52c280f74
Burak Kakillioglu <bkakilli@syr.edu>
2020-05-19 01:59:03 -0400

    scannet script coding style + valid set (random)

--- scripts/scannet_dataloader.py
+++ scripts/scannet_dataloader.py
@@ -1,12 +1,12 @@
 import numpy as np
 import torch
 import os
-import torch.utils.data as data
+import torch.utils.data as torch_data
 
 np.random.seed(0)
 
-class ScanNetDataset(data.Dataset):
-    def __init__(self,root,split,path_prefix=None,augmentation=None):
+class ScanNetDataset(torch_data.Dataset):
+    def __init__(self, root, split, augmentation=None):
         self.cls_list= [
         'floor', 
         'wall', 
@@ -29,8 +29,8 @@ class ScanNetDataset(data.Dataset):
         'sink', 
         'otherprop']
         
-        self.root=root
-        self.split=split
+        self.root = root
+        self.split = split
         
         self.batch_list=self.create_batch_list()        
         
@@ -38,26 +38,26 @@ class ScanNetDataset(data.Dataset):
         
         
     def create_batch_list(self):
-        if self.split=='train':
-            folder='scans'
-        elif self.split=='test':
-            folder='scans_test'
+        if self.split == 'train':
+            folder = 'scans'
+        elif self.split == 'test':
+            folder = 'scans_test'
         
-        all_batch_list=[]
-        room_path=os.path.join(self.root,folder)
-        room_list=os.listdir(room_path)
+        all_batch_list = []
+        room_path = os.path.join(self.root, folder)
+        room_list = os.listdir(room_path)
         for room in room_list:
-            batch_folder_path=os.path.join(room_path,room,'batch_folder')
+            batch_folder_path = os.path.join(room_path, room, 'batch_folder')
             
-            #randomly pick up 5 batch for each room
-            batch_list=os.listdir(batch_folder_path)
-            picked_value=5
-            if picked_value>len(batch_list):
-                picked_value=len(batch_list)
+            # Randomly pick up 5 batch for each room
+            batch_list = os.listdir(batch_folder_path)
+            picked_value = 5
+            if picked_value > len(batch_list):
+                picked_value = len(batch_list)
 
-            batch_indice=np.random.choice(np.arange(len(batch_list)),picked_value,replace=False)
+            batch_indice = np.random.choice(np.arange(len(batch_list)), picked_value, replace=False)
             for indice in batch_indice:
-                picked_batch=os.path.join(batch_folder_path,batch_list[indice])
+                picked_batch = os.path.join(batch_folder_path, batch_list[indice])
                 all_batch_list.append(picked_batch)
         
         return all_batch_list
@@ -66,27 +66,35 @@ class ScanNetDataset(data.Dataset):
         return len(self.batch_list)
     
     def __getitem__(self,batch_index):
-        txt_file=self.batch_list[batch_index]
-        data=np.loadtxt(txt_file)
-        inpt=torch.FloatTensor(data[:,0:9])
-        label=torch.LongTensor(data[:,-1])
-        return inpt,label
+        txt_file = self.batch_list[batch_index]
+        data = np.loadtxt(txt_file)
+        inpt = torch.FloatTensor(data[:,0:9])
+        label = torch.LongTensor(data[:,-1])
+        return inpt, label
     
-def get_sets(data_folder, path_prefix=None, training_augmentation=True):
-    """Return hooks to S3DIS dataset train, validation and tests sets.
+def get_sets(data_folder, training_augmentation=True):
+    """Return hooks to ScanNet dataset train, validation and tests sets.
     """
 
-    train_set = ScanNetDataset(data_folder, split='train',path_prefix=path_prefix, augmentation=training_augmentation)
-    # valid_set = S3DISDataset(data_folder, split='val', path_prefix=path_prefix)
-    # test_set = S3DISDataset(data_folder, split='test', path_prefix=path_prefix)
+    train_set = ScanNetDataset(data_folder, split='train', augmentation=training_augmentation)
+
+    train_ratio = 0.9
+    train_split = int(len(train_set)*train_ratio)
+    valid_split = len(train_set) - train_split
+    
+    train_set, valid_set = torch_data.random_split(train_set, (train_split, valid_split))
+    test_set = ScanNetDataset(data_folder, split='test') 
 
     return train_set
 
 
+def test():
+    dataloader = ScanNetDataset('/data1/datasets/ScanNet/DATA', split='train') 
+    inpt, oupt = dataloader[3] 
+
 
             
-if __name__=='__main__':
-    dataloader=ScanNetDataset('/data1/datasets/ScanNet/DATA',split='train') 
-    inpt,oupt=dataloader[3] 
+if __name__== '__main__':
+    test()
     
     
